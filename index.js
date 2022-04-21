ed8d7134c819f388efa28ed200fdd9b8b27ce4e7
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-04-20 23:43:50 -0300

    get request done

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+
+const app = express();
+const tweets = [
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	},
+    {
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	    tweet: "eu amo o hub"
+	}
+]
+
+app.get("/tweets", (req, res) => {
+    let tenTweets = tweets.slice(-10);
+    res.send(tenTweets);
+})
+
+app.listen(5000, () => console.log("server is running on port 5000"));
