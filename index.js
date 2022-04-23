7e73b6653cd1140e1667157b65c54905469c8c54
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-04-23 20:24:39 -0300

    project working

--- index.js
+++ index.js
@@ -1,61 +1,35 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const tweets = [
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	    tweet: "eu amo o hub"
+const tweets = [];
+const users = [];
+const map = new Map();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/sign-up", (req, res) => {
+	const body = req.body;
+	users.push(body);
+	map.set(`${body.username}`, body.avatar);
+	res.send("Ok");
+})
+
+app.post("/tweets", (req, res) => {
+	const body = req.body;
+	const tweet = {
+		username: body.username,
+		avatar: map.get(`${body.username}`),
+		tweet: body.tweet,
 	}
-]
+	tweets.push(tweet);
+	res.send("Ok");
+})
 
 app.get("/tweets", (req, res) => {
     let tenTweets = tweets.slice(-10);
+	tenTweets = tenTweets.reverse();
     res.send(tenTweets);
 })
 
